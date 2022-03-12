cde120bf80374b503e9a59751602ddee18aa02a1
Carlos Aguilera <18aguilerac@gmail.com>
2022-03-12 12:21:01 -0800

    update

--- Template.cpp
+++ Template.cpp
@@ -7,42 +7,37 @@
  * DUE DATE    : 
  ****************************************************************************/
 
-#include <iostream>
-#include <iomanip>
-
 /****************************************************************************
  * Title:
  * --------------------------------------------------------------------------
- * This program will output the class heading
+ * FUNCTION:
  * --------------------------------------------------------------------------
- * INPUT:
- *
- * OUTPUT:
+ * Data Table
+ * ----------
  ***************************************************************************/
-int main() {
-	 /***********************************************************************
-	  * CONSTANTS
-	  * ---------------------------------------------------------------------
-	  * OUTPUT - USED FOR CLASS HEADING
-	  * ---------------------------------------------------------------------
-	  * PROGRAMMER : Programmer's Name
-	  * CLASS      : Student's Course
-	  * SECTION    : Class Days and Times
-	  * LAB_NUM    : Lab Number (specific to this lab)
-	  * LAB_NAME   : Title of the Lab
-	  **********************************************************************/
+
+#include <iostream>
+#include <iomanip>
+
+void heading() {
+
+	/***********************************************************************
+	 * CONSTANTS
+	 * ---------------------------------------------------------------------
+	 * OUTPUT - USED FOR CLASS HEADING
+	 * ---------------------------------------------------------------------
+	 * PROGRAMMER : Programmer's Name
+	 * CLASS      : Student's Course
+	 * SECTION    : Class Days and Times
+	 * LAB_NUM    : Lab Number (specific to this lab)
+	 * LAB_NAME   : Title of the Lab
+	 **********************************************************************/
 	const char PROGRAMMER[] = "Carlos Aguilera";
 	const char CLASS[]      = "CS1B";
 	const char SECTION[]    = "MW: 7:30p - 9:50p";
 	const int LAB_NUM       = 1;
 	const char LAB_NAME[]   = "";
 
-	// (variable declerations go here)
-
-
-	/************************************************************************
-	 * OUTPUT - Class Heading
-	 ***********************************************************************/
 	std::cout << std::left;
 	std::cout << "****************************************************\n";
 	std::cout << "*   PROGRAMMED BY : " << PROGRAMMER << std::endl;
