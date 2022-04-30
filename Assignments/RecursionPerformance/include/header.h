829a1814353128f110f20026ff5d65013ca06b68
Carlos Aguilera <18aguilerac@gmail.com>
2022-04-30 10:11:17 -0700

    Added both implementations of raw testing and output testing

--- Assignments/RecursionPerformance/include/header.h
+++ Assignments/RecursionPerformance/include/header.h
@@ -3,6 +3,13 @@
 #include <iostream>
 #include <iomanip>
 
+enum MenuOption {
+  Exit = 0,
+  DisplayFactorial,
+  DisplayFibonacci,
+  FactorialPerformance,
+  FibonacciPerformance
+};
 /*******************************************************************************
  * 
  ------------------------------------------------------------------------------- 
@@ -14,5 +21,11 @@ void printMenu();
 void printHeading();
 void iterativeFactorial(long long &n);
 long long recursiveFactorial(long long n);
+long long recursiveFib(long long series);
+void iterativeFib(const int &series);
+long long recursiveFibRaw(long long series);
+void iterativeFibRaw(const int &series);
+void iterativeFactorialRaw(long long &n);
+long long recursiveFactorialRaw(long long n);
 
 #endif // HEADER_H_
