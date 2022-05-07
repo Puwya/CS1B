fd29ffa2d6bb1e772cc630f22845ec6bd7948929
Carlos Aguilera <18aguilerac@gmail.com>
2022-05-06 18:51:16 -0700

    implemented header, menu, and .h files

---
+++ Labs/ArrayOfSheeps/src/SheepList.cpp
@@ -0,0 +1,9 @@
+#include "../include/SheepList.h"
+
+void SheepList::addSheep(const Sheep &newSheep) {}
+void SheepList::clear() {}
+
+void SheepList::size() const {}
+bool SheepList::find() const {}
+void SheepList::list() const {}
+void SheepList::firstSheep() const {}
